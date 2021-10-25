dbb566bedbc2bf9d07e3a3de21becea405a88734
Matias Jara <mati.jarag@gmail.com>
2021-10-25 13:18:01 -0300

    add Test Password

--- src/test/java/es/springboot/bci/PruebaBciRestApplicationTests.java
+++ src/test/java/es/springboot/bci/PruebaBciRestApplicationTests.java
@@ -1,6 +1,5 @@
 package es.springboot.bci;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import es.springboot.bci.controller.UsuarioController;
@@ -19,10 +18,7 @@ class PruebaBciRestApplicationTests {
 	
 	UsuarioController methods = new UsuarioController();
 	
-	@Test
-	void contextLoads() {
-	}
-	
+	/*TEST EMAIL*/
 	@ParameterizedTest(name = "#{index} - Run test with email = {0}")
     @MethodSource("validEmailProvider")
     void test_email_regex_valid(String email) {
@@ -35,6 +31,21 @@ class PruebaBciRestApplicationTests {
         assertFalse(methods.checkEmailFormat(email));
     }
 	
+	
+	/*TEST PASSWORD*/
+	@ParameterizedTest(name = "#{index} - Run test with password = {0}")
+    @MethodSource("validPasswordProvider")
+    void test_password_regex_valid(String password) {
+        assertTrue(methods.checkPasswordFormat(password));
+    }
+	
+	@ParameterizedTest(name = "#{index} - Run test with password = {0}")
+    @MethodSource("invalidPasswordProvider")
+    void test_password_regex_invalid(String password) {
+        assertFalse(methods.checkPasswordFormat(password));
+    }
+	
+	/*Ejecucion pruebas Email*/
 	static Stream<String> validEmailProvider(){
 		return Stream.of(
 				"aaaa@gmail.com",
@@ -53,5 +64,28 @@ class PruebaBciRestApplicationTests {
 				"wrongemail@com."
 				);
 	}
-
+	
+	/*Ejecucion pruebas Password*/
+	static Stream<String> validPasswordProvider(){
+		return Stream.of(
+				"123ABc._",
+				"AAA!f#$=1",
+				"0230420342$gdHGbdfAA",
+				"Asd123$h"
+				);
+	}
+	
+	static Stream<String> invalidPasswordProvider(){
+		return Stream.of(
+				"12334563456",
+				"abcdefghijkl",
+				"ABXCDSEGHSDG",
+				"abcd12345$",
+				"ABC34562345",
+				"dfgd2345ABCD",
+				"_______",
+				" ",
+				""
+				);
+	}
 }
